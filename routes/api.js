1fc0a462c81db4fa1938344e16586b6e0536fa53
Jakub Macina <jakub.macina@gmail.com>
2015-11-14 16:40:11 +0100

    Add nearest distance ordering

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,10 @@ var dbFactory = require('../db-config');
 
 exports.identify = function(req, res){
   console.log("Getting nearest schools");
-  var query = "SELECT *, st_asgeojson(way) as geojson FROM schools s WHERE st_dwithin(ST_SetSRID(ST_MakePoint($1, $2),4326)::geography,s.way::geography, 1000)";
+  var query = "SELECT *, st_asgeojson(way) as geojson, ST_Distance(ST_SetSRID(ST_MakePoint($1, $2),4326)::geography,s.way::geography, false) as distance " +
+      "FROM schools s " +
+      "WHERE st_dwithin(ST_SetSRID(ST_MakePoint($1, $2),4326)::geography,s.way::geography, 1000)" +
+      "ORDER BY distance";
   return dbFactory.db.query(query, [req.query.lon, req.query.lat ]).then(function(results){
       return res.send(results);
   }).catch(function (e) {
